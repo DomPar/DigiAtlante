5f2aa2be288041b49049951170c9e828367e90d3
DomPar <djps94@outlook.com>
2024-03-25 10:28:04 +0000

    digi router

--- backend/api/routers/routers.js
+++ backend/api/routers/routers.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-
 const userRouter = require('./user.router')
 const authRouter = require('./auth.router')
+const digiRouter = require('./digimon.router')
 
 router.use('/auth', authRouter)
 router.use('/user', userRouter)
+router.use('/digimon', userRouter)
+
 
 
 module.exports = router
