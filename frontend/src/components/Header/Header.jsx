12844bed7ad00c8d7a84c67b4d97797b30c94e36
Aaron <ravycrow@gmail.com>
2024-03-27 16:22:59 +0000

    navigate funciona

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import './Header.css'
+import { useNavigate } from 'react-router-dom';
+import './Header.css';
+import LoginIcon from '@mui/icons-material/Login';
+
 const Header = () => {
-    const navigate = useNavigate();
-    return (
+  const navigate = useNavigate();
+
+  const handleClick = (path) => {
+    navigate(path);
+  };
+
+  return (
     <div id='containerHeader' className='flex items-center bg-[#ffb6c1] justify-around'>
-        <div id='logo'>
-        <button onClick={navigate('/')} className='w-56 h-56'>
+      <div id='logo'>
+        <button onClick={() => handleClick('/')} className='w-56 h-56'>
           <img src="\image\logo.png" alt="logo" />
         </button>
-        </div>
-        <div id='navbar' className=''>
-         <button onClick={navigate('/')}>DigiDex</button>
-         <button onClick={navigate('/')}>API</button>
-         <button onClick={navigate('/')}>About Us</button>
-        </div>
+      </div>
+      <div id='navbar' className='flex gap-8 flex-row'>
+        <button onClick={() => handleClick('/file')} className=''>DigiDex</button>
+        <button onClick={() => handleClick('/')}>API</button>
+        <button onClick={() => handleClick('/')}>About Us</button>
+        <button onClick={() => handleClick('/')}>Create</button>
+        <button onClick={() => handleClick('/')}>Upload</button>
+      </div>
+      <div>
+        <button onClick={() => handleClick('/')}><LoginIcon /></button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
