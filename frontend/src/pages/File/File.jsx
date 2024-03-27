bd6683c109b1c17f91dabe4dc5fba4f1b9fddb49
DomPar <djps94@outlook.com>
2024-03-27 12:12:43 +0000

    csseando

--- frontend/src/pages/File/File.jsx
+++ frontend/src/pages/File/File.jsx
@@ -3,8 +3,21 @@ import React from 'react'
 
 const File = () => {
   return (
-    <div id='containerFile' className='w-full h-full bg-orange-100'>
-      
+    <div id='containerFile' className='w-full h-full bg-orange-100 flex'>
+      <div id="digiImage" className='bg-green-100 w-full h-full'>
+
+      </div>
+      <div id="digiInfo" className='bg-cyan-100 w-full h-full flex-column gap-10'>
+        <div id="digiNombre" className='h-10 w-full'>Nombre</div>
+        <div id="digiLevel" className='h-10 w-full'>Level</div>
+        <div id="digiType" className='h-10 w-full'>Type</div>
+        <div id="digiAtributte" className='h-10 w-full'>Attribute</div>
+        <div id="digiAttack" className='h-10 w-full'>Attack</div>
+        <div id="digiDescription" className='h-40 w-full'>Description description Description description Description description Description description Description description Description description Description description Description description</div>
+        <div id="preEvoDigi" className='h-100 w-full bg-orange-100 border-solid'></div>
+        <div id="evoDigi" className='h-100 w-full bg-orange-100 border-solid'></div>
+        <div id="variantDigi" className='h-100 w-full bg-orange-100 border-solid'></div>
+      </div>      
     </div>
   )
 }
