6479127dd61ef6da27af972e7d0d38f2441edaa8
Aaron <ravycrow@gmail.com>
2024-03-27 17:59:32 +0000

    aboutus

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
       <div id='navbar' className='flex gap-8 flex-row'>
         <button onClick={() => handleClick('/file')} className=''>DigiDex</button>
         <button onClick={() => handleClick('/')}>API</button>
-        <button onClick={() => handleClick('/')}>About Us</button>
+        <button onClick={() => handleClick('/aboutus')}>About Us</button>
         <button onClick={() => handleClick('/')}>Create</button>
         <button onClick={() => handleClick('/')}>Upload</button>
       </div>
