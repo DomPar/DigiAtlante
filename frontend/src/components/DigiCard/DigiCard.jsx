d22316bde9599b03ecc227c3a7111b8466e22c42
DomPar <djps94@outlook.com>
2024-03-28 10:44:54 +0000

    sas bobo

--- frontend/src/components/DigiCard/DigiCard.jsx
+++ frontend/src/components/DigiCard/DigiCard.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
 
 const DigiCard = ({id, name, image}) => {
 
 
-
   return (
     <div >
-      <div id="containerCard" className="flex flex-col items-center">
-        <img src={image} alt="imagen digimon" className=""/>
-        <p>{name}</p>
-      </div>
+      <Link to={`/file/${id}`}>
+        <div id="containerCard" className="flex flex-col items-center">
+          <img src={image} alt="imagen digimon" className=""/>
+          <p>{name}</p>
+        </div>
+      </Link>
     </div>
   )
 }
