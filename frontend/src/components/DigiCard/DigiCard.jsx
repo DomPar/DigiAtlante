3ec171644a3f8706f7ef7123b295b8e1feabd1d8
Aaron <ravycrow@gmail.com>
2024-03-28 12:14:01 +0000

    working hard

--- frontend/src/components/DigiCard/DigiCard.jsx
+++ frontend/src/components/DigiCard/DigiCard.jsx
@@ -7,10 +7,10 @@ const DigiCard = ({id, name, image}) => {
   return (
     <div >
       <Link to={`/file/${id}`}>
-        <div id="containerCard" className="flex flex-col items-center">
-          <img src={image} alt="imagen digimon" className=""/>
-          <p>{name}</p>
-        </div>
+          <div id="containerCard" className="flex flex-col items-center border-2 border-[#d06b8e] pb-2 bg-[#d06b8e]">
+            <img src={image} alt="imagen digimon" className=""/>
+            <p className="text-white uppercase pt-0.5">{name}</p>
+          </div>
       </Link>
     </div>
   )
