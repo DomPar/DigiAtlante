be7ec6e155923afb3f22b6a615382b6491024c9b
Aaron <ravycrow@gmail.com>
2024-03-31 12:07:42 +0100

    Welcome

--- frontend/src/routers/index_router.jsx
+++ frontend/src/routers/index_router.jsx
@@ -1,13 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../pages/App/App.jsx";
-import Home from "../pages/Home/Home.jsx";
 import File from "../pages/File/File.jsx";
 import AboutUs from "../pages/AboutUs/AboutUs.jsx";
 import Digidex from "../pages/Digidex/Digidex.jsx";
 import Create from "../pages/Create/Create.jsx";
 import Login from "../pages/Login/Login.jsx";
 import Relate from "../pages/Relate/Relate.jsx";
-
+import Welcome from "../pages/Welcome/Welcome.jsx";
 const router = createBrowserRouter([
     {
         path: '/',
@@ -15,7 +14,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                element: <Home/>
+                element: <Welcome/>
             },
             {
                 path: '/file/:digiId',
@@ -40,7 +39,8 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login/>
-            }
+            },
+          
         ]
     } 
 ])
