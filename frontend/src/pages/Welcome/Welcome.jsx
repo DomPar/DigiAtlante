715082463f3dbe17baf1981e06af4b81c2d4b55c
DomPar <djps94@outlook.com>
2024-04-02 11:06:35 +0100

    2

--- frontend/src/pages/Welcome/Welcome.jsx
+++ frontend/src/pages/Welcome/Welcome.jsx
@@ -32,18 +32,6 @@ const Welcome = () => {
     </div>
   );
 };
-        <div>
-          {/* Bienvenida */}
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vitae neque dictum, ultrices tellus volutpat, auctor tortor. Cras convallis luctus. 
-        </div>
-        <div>
-          {/* EXplicacion sencilla de la digidex y boton de enlace*/}
-        </div>
-        <div>
-          {/* Explicacion sencilla de la api y boton enlace */}
-        </div>
-    </div>
-  )
-}
+       
 
 export default Welcome;
