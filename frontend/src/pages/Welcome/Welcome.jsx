bad9d502c8cd1fb0f89d8e993888ac637703b6ce
Aaron <ravycrow@gmail.com>
2024-04-04 15:58:27 +0100

    welcome updated

--- frontend/src/pages/Welcome/Welcome.jsx
+++ frontend/src/pages/Welcome/Welcome.jsx
@@ -1,37 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Welcome.css'; 
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const Welcome = () => {
-  const [showSide1, setShowSide1] = useState(true)
-
-  const toggleSide = () => {
-    setShowSide1(!showSide1) 
-  };
-
+  
   return (
-    <div id='containerWelcome' >
-      <div className={`side ${showSide1 ? 'show-side1' : 'show-side2'}`}>
-        {showSide1 ? (
-          <div id='side1'>
-            {/* Contenido del lado 1 */}
-            <h1 className='welcomeH1'>Welcome To Digital World</h1>
-            <button className='leftArrow' onClick={toggleSide}><ArrowBackIosIcon/></button>
-            <button className='rightArrow' onClick={toggleSide}><ArrowForwardIosIcon/></button> 
-          </div>
-        ) : (
-          <div id='side2'>
-            {/* Contenido del lado 2 */}
-            <h1 className='welcomeH1'>Welcome To Digital World</h1>
-            <button className='leftArrow' onClick={toggleSide}><ArrowBackIosIcon/></button>
-            <button className='rightArrow' onClick={toggleSide}><ArrowForwardIosIcon/></button> 
-          </div>
-        )}
+    <main id='containerWelcome' className='relative h-full'>
+      <div id='info' className='absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center '>
+        <h1 className='text-4xl text-white uppercase'>Welcome To Digital World</h1> 
       </div>
-    </div>
+      <video autoPlay loop muted className='absolute inset-0 w-full h-full object-cover'>
+        <source src='/animation/abandoned-Train.mp4' type='video/mp4' />
+      </video>
+      <div id='layer' className='absolute inset-0 bg-[#1c1c1d] opacity-30 mix-blend-overlay'></div>
+    </main> 
   );
 };
-       
 
 export default Welcome;
