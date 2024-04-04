7532bef8cbad22739fecf9a128cd0ec8fd28d7a0
DomPar <djps94@outlook.com>
2024-04-04 16:23:24 +0100

    new bg

--- frontend/src/pages/Welcome/Welcome.jsx
+++ frontend/src/pages/Welcome/Welcome.jsx
@@ -5,13 +5,13 @@ const Welcome = () => {
   
   return (
     <main id='containerWelcome' className='relative h-full'>
-      <div id='info' className='absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center '>
+      {/* <div id='info' className='absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center '>
         <h1 className='text-4xl text-white uppercase'>Welcome To Digital World</h1> 
       </div>
       <video autoPlay loop muted className='absolute inset-0 w-full h-full object-cover'>
         <source src='/animation/abandoned-Train.mp4' type='video/mp4' />
       </video>
-      <div id='layer' className='absolute inset-0 bg-[#1c1c1d] opacity-30 mix-blend-overlay'></div>
+      <div id='layer' className='absolute inset-0 bg-[#1c1c1d] opacity-30 mix-blend-overlay'></div> */}
     </main> 
   );
 };
