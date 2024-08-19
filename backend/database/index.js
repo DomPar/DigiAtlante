4ee51efb363fd11e0b4e894bd32fc4696965d060
DomPar <djps94@outlook.com>
2024-08-19 22:52:33 +0100

    db.port

--- backend/database/index.js
+++ backend/database/index.js
@@ -3,7 +3,8 @@ const { Sequelize } = require('sequelize');
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
 host: process.env.DB_HOST,
 dialect: process.env.DB_DIALECT,
-logging: false
+logging: false,
+port: process.env.DB_PORT,
 });
 
 const checkDB = async () => {
